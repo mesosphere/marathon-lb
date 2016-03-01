6d0fb45f94c8c556e27f07bbee259cfb276200eb
ajays20078 <ajays20078@gmail.com>
2016-03-01 22:19:25 +0530

    added support for haproxy groups  per service group along with test cases and documentation

--- tests/test_marathon_lb.py
+++ tests/test_marathon_lb.py
@@ -695,3 +695,139 @@ backend nginx_10000
   server 1_1_1_1_1024 1.1.1.1:1024 check inter 2s fall 11
 '''
         self.assertMultiLineEqual(config, expected)
+
+    def test_config_haproxy_group_fallback(self):
+        apps = dict()
+        groups = ['external']
+        bind_http_https = True
+        ssl_certs = ""
+        templater = marathon_lb.ConfigTemplater()
+
+        healthCheck = {}
+        app1 = marathon_lb.MarathonService('/nginx', 10000, healthCheck)
+        app1.groups = ['external', 'internal']
+        app1.add_backend("1.1.1.1", 1024, False)
+        app2 = marathon_lb.MarathonService('/nginx', 10001, healthCheck)
+        app2.groups = ['external', 'internal']
+        app2.add_backend("1.1.1.1", 1025, False)
+        apps = [app1, app2]
+        config = marathon_lb.config(apps, groups, bind_http_https,
+                                    ssl_certs, templater)
+        expected = self.base_config + '''
+frontend marathon_http_in
+  bind *:80
+  mode http
+
+frontend marathon_http_appid_in
+  bind *:9091
+  mode http
+
+frontend marathon_https_in
+  bind *:443 ssl crt /etc/ssl/mesosphere.com.pem
+  mode http
+
+frontend nginx_10000
+  bind *:10000
+  mode tcp
+  use_backend nginx_10000
+
+frontend nginx_10001
+  bind *:10001
+  mode tcp
+  use_backend nginx_10001
+
+backend nginx_10000
+  balance roundrobin
+  mode tcp
+  server 1_1_1_1_1024 1.1.1.1:1024
+
+backend nginx_10001
+  balance roundrobin
+  mode tcp
+  server 1_1_1_1_1025 1.1.1.1:1025
+'''
+        self.assertMultiLineEqual(config, expected)
+
+    def test_config_haproxy_group_per_service(self):
+        apps = dict()
+        groups = ['external']
+        bind_http_https = True
+        ssl_certs = ""
+        templater = marathon_lb.ConfigTemplater()
+
+        healthCheck = {}
+        app1 = marathon_lb.MarathonService('/nginx', 10000, healthCheck)
+        app1.haproxy_groups = ['external']
+        app1.add_backend("1.1.1.1", 1024, False)
+        app2 = marathon_lb.MarathonService('/nginx', 10001, healthCheck)
+        app2.haproxy_groups = ['internal']
+        app2.add_backend("1.1.1.1", 1025, False)
+        apps = [app1, app2]
+        config = marathon_lb.config(apps, groups, bind_http_https,
+                                    ssl_certs, templater)
+        expected = self.base_config + '''
+frontend marathon_http_in
+  bind *:80
+  mode http
+
+frontend marathon_http_appid_in
+  bind *:9091
+  mode http
+
+frontend marathon_https_in
+  bind *:443 ssl crt /etc/ssl/mesosphere.com.pem
+  mode http
+
+frontend nginx_10000
+  bind *:10000
+  mode tcp
+  use_backend nginx_10000
+
+backend nginx_10000
+  balance roundrobin
+  mode tcp
+  server 1_1_1_1_1024 1.1.1.1:1024
+'''
+        self.assertMultiLineEqual(config, expected)
+
+    def test_config_haproxy_group_hybrid(self):
+        apps = dict()
+        groups = ['external']
+        bind_http_https = True
+        ssl_certs = ""
+        templater = marathon_lb.ConfigTemplater()
+
+        healthCheck = {}
+        app1 = marathon_lb.MarathonService('/nginx', 10000, healthCheck)
+        app1.haproxy_groups = ['internal']
+        app1.add_backend("1.1.1.1", 1024, False)
+        app2 = marathon_lb.MarathonService('/nginx', 10001, healthCheck)
+        app2.groups = ['external']
+        app2.add_backend("1.1.1.1", 1025, False)
+        apps = [app1, app2]
+        config = marathon_lb.config(apps, groups, bind_http_https,
+                                    ssl_certs, templater)
+        expected = self.base_config + '''
+frontend marathon_http_in
+  bind *:80
+  mode http
+
+frontend marathon_http_appid_in
+  bind *:9091
+  mode http
+
+frontend marathon_https_in
+  bind *:443 ssl crt /etc/ssl/mesosphere.com.pem
+  mode http
+
+frontend nginx_10001
+  bind *:10001
+  mode tcp
+  use_backend nginx_10001
+
+backend nginx_10001
+  balance roundrobin
+  mode tcp
+  server 1_1_1_1_1025 1.1.1.1:1025
+'''
+        self.assertMultiLineEqual(config, expected)
