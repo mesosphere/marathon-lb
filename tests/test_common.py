732905587814aad417e0a11504d2603fd98b6fcb
James Morelli <james@wesome.nyc>
2016-06-03 16:15:04 -0400

    use /dev/null in test_common.py for logging

--- tests/test_common.py
+++ tests/test_common.py
@@ -8,7 +8,7 @@ class TestCommon(unittest.TestCase):
     def test_setup_logging_log_level(self):
         logger = Mock()
         logger.INFO = 'BAR'
-        common.setup_logging(logger, '/var/run/syslog',
+        common.setup_logging(logger, '/dev/null',
                              '%(name)s: %(message)s', 'info')
 
         logger.setLevel.assert_called_with('BAR')
@@ -16,7 +16,7 @@ class TestCommon(unittest.TestCase):
     def test_setup_logging_invalid_log_level(self):
         logger = Mock()
         with self.assertRaises(Exception) as context:
-            common.setup_logging(logger, '/var/run/syslog',
+            common.setup_logging(logger, '/dev/null',
                                  '%(name)s: %(message)s', 'banana')
 
         assert str(context.exception) == 'Invalid log level: BANANA'
