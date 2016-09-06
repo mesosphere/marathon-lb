d2a6f25cadb697367417e0c2ab2524e9d45a71c4
Jamie Hewland <jhewland@gmail.com>
2016-09-06 23:46:08 +0200

    HAProxy build script and Dockerfile improvements (#304)
    * Check checksums for Lua/HAProxy and set versions in variables
    
    * Don't install docs and manpages for HAProxy
    
    * Use same tar flags for Lua + HAProxy
    
    * Dockerfile: put build and run deps in alphabetical variables
    
    * set -x on the long RUN command
    
    * Dockerfile: some of these runtime deps are actually build deps
    
    * Dockerfile: dateutil is installed by pip
    
    * Dockerfile: install individual build tools instead of build-essentials
    
    * Dockerfile: install with --no-install-recommends to avoid unneeded packages
    
    * Dockerfile: remove APT indexes after installing
    
    * Dockerfile: running apt-get clean isn't necessary
    
    * See this discussion:
      https://github.com/docker-library/buildpack-deps/pull/41
    
    * Dockerfile: apt-get purge instead of just remove
    
    * Can auto-remove here too
    * If you're forcing you're probably doing it wrong
    
    * Dockerfile: use --no-cache flag with pip
    
    * Dockerfile: don't need wheel if we're not caching them
    
    * Bump HAProxy version to 1.6.9

--- build-haproxy.sh
+++ build-haproxy.sh
@@ -4,18 +4,29 @@ set -e
 mkdir -p /usr/src
 
 # Build Lua
+LUA_VERSION="5.3.3"
+LUA_SHA1="a0341bc3d1415b814cc738b2ec01ae56045d64ef"
+
 cd /usr/src
-wget http://www.lua.org/ftp/lua-5.3.3.tar.gz
-tar zxf lua-*.tar.gz
-cd lua-*
+LUA_FILENAME="lua-$LUA_VERSION"
+wget "http://www.lua.org/ftp/$LUA_FILENAME.tar.gz"
+echo "$LUA_SHA1  $LUA_FILENAME.tar.gz" | sha1sum -c
+tar zxf "$LUA_FILENAME.tar.gz"
+cd "$LUA_FILENAME"
 make -j4 linux LUA_LIB_NAME=lua53
 make -j4 install LUA_LIB_NAME=lua53
 
 # Build HAProxy
+HAPROXY_MAJOR_VERSION="1.6"
+HAPROXY_VERSION="1.6.9"
+HAPROXY_MD5="c52eee40eb66f290d6f089c339b9d2b3"
+
 cd /usr/src
-wget http://www.haproxy.org/download/1.6/src/haproxy-1.6.7.tar.gz
-tar xf haproxy-*.tar.gz
-cd haproxy-*
+HAPROXY_FILENAME="haproxy-$HAPROXY_VERSION"
+wget "http://www.haproxy.org/download/$HAPROXY_MAJOR_VERSION/src/$HAPROXY_FILENAME.tar.gz"
+echo "$HAPROXY_MD5  $HAPROXY_FILENAME.tar.gz" | md5sum -c
+tar zxf "$HAPROXY_FILENAME.tar.gz"
+cd "$HAPROXY_FILENAME"
 make -j4 \
   TARGET=custom \
   CPU=x86_64 \
@@ -31,7 +42,7 @@ make -j4 \
   LUA_INC=/usr/local/include/ \
   USE_ZLIB=1 \
   LDFLAGS="-lcrypt  -lssl -lcrypto -L/usr/local/lib/ -llua -lm -L/usr/lib -lpcreposix -lpcre"
-make -j4 install LDFLAGS="-lcrypt  -lssl -lcrypto -L/usr/local/lib/ -llua -lm -L/usr/lib -lpcreposix -lpcre -ldl"
+make -j4 install-bin LDFLAGS="-lcrypt  -lssl -lcrypto -L/usr/local/lib/ -llua -lm -L/usr/lib -lpcreposix -lpcre -ldl"
 
 # Clean up
 cd /
