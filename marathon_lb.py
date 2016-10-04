8eec59cbcb55eea91e036751627b868129de0634
Ole Bakstad <ole.bakstad@knowit.no>
2016-10-04 07:05:19 +0200

    Add printout for debugging

--- marathon_lb.py
+++ marathon_lb.py
@@ -660,6 +660,7 @@ def reloadConfig():
             subprocess.check_call(reloadCommand, close_fds=True)
             # Wait until the reload actually occurs and there's a new PID
             while len(get_haproxy_pids() - old_pids) < 1:
+                logger.info("Waiting for new haproxy pid...") # TODO: remove. Only for debugging.
                 time.sleep(0.1)
             logger.debug("reload finished, took %s seconds",
                          time.time() - start_time)
