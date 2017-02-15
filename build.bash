8550fdf00dade4f20f7d3ad530e99e95d3069014
Jamie Hewland <jhewland@gmail.com>
2017-02-15 11:30:15 +0200

    Run flake8 after builds

--- build.bash
+++ build.bash
@@ -13,3 +13,6 @@ $PIPCMD install -r requirements-dev.txt
 PYTHONPATH=. coverage run "$(which nosetests)"
 
 coverage report
+
+# Allow flake8 to fail-- too many errors currently but they're good to know
+flake8 . || true
